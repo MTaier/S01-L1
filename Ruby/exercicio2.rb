9c399ef351863b4a7cc4565b9357c3acced583de
Marcio Taier <128880383+MTaier@users.noreply.github.com>
2024-10-07 22:58:06 -0300

    aula 7 - exercicios

---
+++ Ruby/exercicio2.rb
@@ -0,0 +1,25 @@
+class Carro
+    def descrever
+      "Este é um carro genérico."
+    end
+end
+  
+class CarroEsportivo < Carro
+    def descrever
+      "Este é um carro esportivo, rápido e potente."
+    end
+end
+  
+class CarroSedan < Carro
+    def descrever
+      "Este é um carro sedan, confortável e espaçoso."
+    end
+end
+
+carro_generico = Carro.new
+carro_esportivo = CarroEsportivo.new
+carro_sedan = CarroSedan.new
+
+puts carro_generico.descrever
+puts carro_esportivo.descrever
+puts carro_sedan.descrever
